3bfe59e88be4273303e397f8b8dac76c4a644084
naveed jamali <naveedjamali@outlook.com>
2021-06-02 16:12:42 +0500

    added children of column and row

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,105 @@ class ColumnRowWidgetApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Column and Row Widget'),),
-    body: Column(children: [
-      Container(
-        width: double.infinity,
-        height: 100,
-        color: Colors.yellow,
-      )
-    ],),);
+    return MaterialApp(
+      title: 'Column and Row Widgets',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Column and Row Widgets'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.purple,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.purple,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.brown,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.indigo,
+                  )
+                ],
+              ),
+              Container(
+                width: double.infinity,
+                height: 30,
+                color: Colors.black,
+              ),Container(
+                width: double.infinity,
+                height: 100,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: double.infinity,
+                height: 100,
+                color: Colors.green,
+              ),
+              Container(
+                width: double.infinity,
+                height: 100,
+                color: Colors.purple,
+              ),
+              Container(
+                width: double.infinity,
+                height: 100,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: double.infinity,
+                height: 100,
+                color: Colors.green,
+              ),
+              Container(
+                width: double.infinity,
+                height: 100,
+                color: Colors.purple,
+              ),
+              Container(
+                width: double.infinity,
+                height: 100,
+                color: Colors.brown,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
